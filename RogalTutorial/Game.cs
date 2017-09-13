f0082cd207b5764fc53806800b41a9c4632c022e
hchecinski <hchecinski@users.noreply.github.com>
2017-09-13 08:53:59 +0200

    Dodanie kolorów

--- RogalTutorial/Game.cs
+++ RogalTutorial/Game.cs
@@ -9,22 +9,75 @@ namespace RogalTutorial
 {
     class Game
     {
-        private static RLRootConsole rootConsole;
-        private static readonly int SCREEN_WIDTH = 100;
-        private static readonly int SCREEN_HIGHT = 70;
+        // The screen height and width are in number of tiles
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+        private static RLRootConsole _rootConsole;
 
-        static void Main(string[] args)
+        // The map console takes up most of the screen and is where the map will be drawn
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+
+        // Below the map console is the message console which displays attack rolls and other information
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        // The stat console is to the right of the map and display player and monster stats
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        // Above the map is the inventory console which shows the players equipment, abilities, and items
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
+
+        public static void Main()
         {
-            rootConsole = new RLRootConsole("terminal8x8.bmp", SCREEN_WIDTH, SCREEN_HIGHT, 14, 8, 1, "Rogal tutorial");
-            rootConsole.Render += rootConsole_Render;
-            rootConsole.Run();
+            _rootConsole = new RLRootConsole("terminal8x8.png", _screenWidth, _screenHeight, 8, 8, 1f, "RougeSharp V3 Tutorial - Level 1");
+
+            // Initialize the sub consoles that we will Blit to the root console
+            _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
+            // Spatrzone z monogame = Update odpowiada za logikę aplikacji
+            _rootConsole.Update += OnRootConsoleUpdate;
+            // Render odpowiada za rysowanie 
+            _rootConsole.Render += OnRootConsoleRender;
+            // Begin RLNET's game loop
+            _rootConsole.Run();
         }
 
-        private static void rootConsole_Render(object sender, UpdateEventArgs e)
+        // odpala się cały czas w tle
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
         {
-            rootConsole.Clear();
-            rootConsole.Print(1, 1, "Witaj Świecie", RLColor.White);
-            rootConsole.Draw();
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, RLColor.Black);
+            _mapConsole.Print(1, 1, "Map", RLColor.White);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, RLColor.Gray);
+            _messageConsole.Print(1, 1, "Messages", RLColor.White);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, RLColor.Brown);
+            _statConsole.Print(1, 1, "Stats", RLColor.White);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
+            _inventoryConsole.Print(1, 1, "Inventory", RLColor.White);
+        }
+
+        // Rysuje, podobno trochę wolnie niż update się odpala
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            // Blit the sub consoles to the root console in the correct locations
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+
+            _rootConsole.Draw();
         }
     }
 }
